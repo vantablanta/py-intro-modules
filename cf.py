d4aeb300b7b0060c1c3c9601c9868c9187946250
vanta blanta <vantablanta@gmail.com>
2022-04-20 16:45:08 +0300

    conditional

---
+++ cf.py
@@ -0,0 +1,15 @@
+# height = int(input("Enter height"))
+# if height > 70 :
+#     print("You are really tall")
+# elif height > 60:
+#     print("You are of average height")
+# else:
+#     print("You are really short")
+
+name = ""
+list_a = []
+
+if list_a:
+    print("I will not run")
+else:
+    print("I am Empty")
